189684f3d1655248152bc8ee040f2261c25cac97
adelwon <kondratyuk.alinaa@gmail.com>
2021-09-28 21:34:14 +0300

    add settlements table

---
+++ database/migrations/2021_09_28_182537_create_settlements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettlementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settlements', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('type');
+            $table->string('description');
+            $table->string('region_description')->nullable();
+            $table->string('area_description');
+            $table->integer('index');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settlements');
+    }
+}
